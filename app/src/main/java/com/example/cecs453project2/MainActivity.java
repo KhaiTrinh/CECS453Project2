672eace5f74a3aee3035ed85ae35dbd97e7725a5
Khai Trinh <khaitrinh99@gmail.com>
2021-06-11 15:53:52 -0700

    Drawables are made into static and can be used inside photo fragment.

--- app/src/main/java/com/example/cecs453project2/MainActivity.java
+++ app/src/main/java/com/example/cecs453project2/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.cecs453project2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int[] animals = {R.drawable.animal13,
+    public static int[] ANIMALS = {R.drawable.animal13,
                                 R.drawable.animal14,
                                 R.drawable.animal15,
                                 R.drawable.animal16,
@@ -22,6 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        photosFrag = PhotosFragment.newInstance();
     }
 }
